e73c89c1a1ed2173cc3748c4bd7f855f84f7aca4
Fathoni <m.fathoni@mail.com>
2020-08-01 09:03:20 +0700

    ignore email.php + add email-example.php untuk admin

---
+++ applications/admin/config/email-example.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['useragent'] = 'CodeIgniter';
+$config['protocol'] = 'smtp';
+$config['email_from'] = 'dikti.puspresnas@kemdikbud.go.id';
+$config['email_from_name'] = 'KIBM';
+
+// Sendmail
+$config['mailpath'] = '/usr/sbin/sendmail';
+
+// SMTP
+$config['smtp_host'] = 'mail.kemdikbud.go.id';
+$config['smtp_user'] = 'dikti.puspresnas@kemdikbud.go.id';
+$config['smtp_pass'] = '';
+$config['smtp_port'] = 587;
+$config['smtp_crypto'] = 'tls';
+$config['smtp_timeout'] = 60;
+$config['smtp_keepalive'] = FALSE;
